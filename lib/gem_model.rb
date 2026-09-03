ea72908a9b035ca6327f348dd8a13919300c1f2e
Dragan Andrei-Valentin <andrei-valentin.dragan@aumovio.com>
2026-09-03 11:26:25 +0300

    refactor: drop redundant GemModel alias and consolidate specs

--- lib/gem_model.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require_relative './gem'
-
-# Compatibility alias for existing references while using a proper Gem class.
-GemModel = RubygemsCli::Gem
--- spec/gem_spec.rb
+++ spec/gem_spec.rb
@@ -13,6 +13,27 @@ RSpec.describe RubygemsCli::Gem do
 
       expect(gems).to all(be_a(described_class))
       expect(gems.map(&:name)).to eq(%w[rails faraday])
+      expect(gems.first.downloads).to eq(1000)
+      expect(gems.first.licenses).to eq(['MIT'])
+      expect(gems.last.info).to eq('HTTP client')
+    end
+
+    it 'applies defaults for missing downloads and licenses' do
+      gem = described_class.new({ 'name' => 'faraday' })
+
+      expect(gem.info).to eq('')
+      expect(gem.downloads).to eq(0)
+      expect(gem.licenses).to eq([])
+    end
+  end
+
+  describe '#matches_name?' do
+    it 'returns true when the gem name contains the keyword' do
+      expect(described_class.new({ 'name' => 'rails' }).matches_name?('rail')).to be(true)
+    end
+
+    it 'returns false when the gem name does not contain the keyword' do
+      expect(described_class.new({ 'name' => 'faraday' }).matches_name?('rails')).to be(false)
     end
   end
 
